1d64cc78d4c9d3cb1f7de1d5f3d8da9c06c55be4
LearnTheABCs <nicholasjtchang@gmail.com>
2020-07-30 20:08:31 -0700

    project finished, everything fully functional

---
+++ tictactoe_driver.py
@@ -0,0 +1,47 @@
+# Tic Tac Toe driver module
+
+from tictactoe_functions import TicTacToeGame
+
+while True:
+    emptyBoard = [' '] * 10
+    ttt = TicTacToeGame(emptyBoard)
+    letter1, letter2 = ttt.inputPlayerLetter()
+    turn = 1
+    ttt.drawBoard()
+    gameIsPlaying = True
+
+    while gameIsPlaying:
+        if turn == 1:
+            print("Player 1's turn.")
+            move = ttt.getPlayerMove()
+            ttt.makeMove(letter1, move)
+            ttt.drawBoard()
+
+            if ttt.isWinner(letter1):
+                print('Player 1 has won the game!')
+                gameIsPlaying = False
+            else:
+                if ttt.isBoardFull():
+                    print('The game is a tie!')
+                    break
+                else:
+                    turn = 2
+
+        else:
+            print("Player 2's turn.")
+            move = ttt.getPlayerMove()
+            ttt.makeMove(letter2, move)
+            ttt.drawBoard()
+
+            if ttt.isWinner(letter2):
+                print('Player 2 has won the game!')
+                gameIsPlaying = False
+            else:
+                if ttt.isBoardFull():
+                    print('The game is a tie!')
+                    break
+                else:
+                    turn = 1
+
+    if not ttt.playAgain():
+        break
