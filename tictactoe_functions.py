4c55ddb74cc7d4ebc3948514d7ae968a40ddcd80
LearnTheABCs <nicholasjtchang@gmail.com>
2020-08-03 11:33:40 -0700

    project finished, everything fully functional

--- tictactoe_functions.py
+++ tictactoe_functions.py
@@ -21,14 +21,14 @@ class TicTacToeGame:
         #print('   |   |')
 
     def inputPlayerLetter(self):
-        # Lets the player type which letter they want to be.
-        # Returns a list with the player's letter as the first item, and the computer's letter as the second.
+        # Lets player 1 type which letter they want to be.
+        # Returns a list with player 1's letter as the first item, and player 2's letter as the second.
         letter = ''
         while not (letter == 'X' or letter == 'O'):
             print('PLAYER 1, CHOOSE X OR O')
             letter = input().upper()
 
-        # the first element in the tuple is the player's letter, the second is the computer's letter.
+        # the first element in the tuple is player 1's letter, the second is player 2's letter.
         if letter == 'X':
             return ['X', 'O']
         else:
